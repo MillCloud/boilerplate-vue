511f9bd67a38eaae4023e95e5a507655407b2444
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-07-09 15:52:05 +0800

    feat: add axios-retry

--- src/composables/useAxios.ts
+++ src/composables/useAxios.ts
@@ -1,9 +1,11 @@
-/* eslint-disable import/prefer-default-export */
 import axios, { AxiosRequestConfig } from 'axios';
 import { useAxios as useIntegrationsAxios } from '@vueuse/integrations';
 import packageInfo from '@/../package.json';
 import { useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
+import axiosRetry from 'axios-retry';
+import statuses from 'statuses';
+import { constantCase } from '@modyqyw/utils';
 import { useToken } from './useToken';
 
 declare interface IRequestConfig extends AxiosRequestConfig {
@@ -14,46 +16,6 @@ const { t } = useI18n();
 const router = useRouter();
 
 const reLaunchCodes = new Set(['TOKEN_OUTDATED']);
-
-const objectStatusCode: { [propName: string]: string } = {
-  400: 'BAD_REQUEST',
-  401: 'UNAUTHORIZED',
-  403: 'FORBIDDEN',
-  404: 'NOT_FOUND',
-  405: 'METHOD_NOT_ALLOWED',
-  406: 'NOT_ACCEPTABLE',
-  407: 'PROXY_AUTHENTICATION_REQUIRED',
-  408: 'REQUEST_TIMEOUT',
-  409: 'CONFLICT',
-  410: 'GONE',
-  411: 'LENGTH_REQUIRED',
-  412: 'PRECONDITION_FAILED',
-  413: 'PAYLOAD_TOO_LARGE',
-  414: 'URI_TOO_LONG',
-  415: 'UNSUPPORTED_MEDIA_TYPE',
-  416: 'RANGE_NOT_SATISFIABLE',
-  417: 'EXPECTATION_FAILED',
-  421: 'MISDIRECTED_REQUEST',
-  422: 'UNPROCESSABLE_ENTITY',
-  423: 'LOCKED',
-  424: 'FAILED_DEPENDENCY',
-  426: 'UPGRADE_REQUIRED',
-  428: 'PRECONDITION_REQUIRED',
-  429: 'TOO_MANY_REQUESTS',
-  431: 'REQUEST_HEADER_FIELDS_TOO_LARGE',
-  451: 'UNAVAILABLE_FOR_LEGAL_REASONS',
-  500: 'INTERNAL_SERVER_ERROR',
-  501: 'NOT_IMPLEMENTED',
-  502: 'BAD_GATEWAY',
-  503: 'SERVICE_UNAVAILABLE',
-  504: 'GATEWAY_TIMEOUT',
-  505: 'HTTP_VERSION_NOT_SUPPORTED',
-  506: 'VARIANT_ALSO_NEGOTIATES',
-  507: 'INSUFFICIENT_STORAGE',
-  508: 'LOOP_DETECTED',
-  510: 'NOT_EXTENDED',
-  511: 'NETWORK_AUTHENTICATION_REQUIRED',
-};
 
 const handleValidateStatusCode = (statusCode: number) =>
   (statusCode >= 200 && statusCode < 300) || statusCode === 304;
@@ -81,6 +43,8 @@ const defaultConfig: AxiosRequestConfig = {
   responseType: 'json',
   validateStatus: handleValidateStatusCode,
 };
+
+axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
 
 axios.interceptors.request.use((config) => ({
   ...config,
@@ -118,8 +82,8 @@ axios.interceptors.response.use(
         // 状态码不正常
         status = JSON.stringify(status);
         response.code = status;
-        response.message = objectStatusCode[status]
-          ? t(`error.${objectStatusCode[status]}`)
+        response.message = statuses(status)
+          ? t(`error.${constantCase(statuses(status).toString())}`)
           : t('error.ERROR_OCCURRED');
       } else {
         // 超时
